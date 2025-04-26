bce8fd1a07b7f9aa82583026a1dab9f9ba538888
haeyong9701 <haeyong9701@gmail.com>
2025-04-26 18:25:36 +0900

    feat: UserInfo 컴포넌트 모바일 반응형

--- src/app/_components/UserInfo/index.tsx
+++ src/app/_components/UserInfo/index.tsx
@@ -1,36 +1,72 @@
 "use client";
 
 import { BasicUserInfo } from "@/app/_type";
+import Image from "next/image";
 import styles from "./UserInfo.module.scss";
+import { formatDate } from "@/app/_lib";
 
 export default function UserInfo({ basicInfo, userName }: { basicInfo: BasicUserInfo; userName: string }) {
+  const lastLogin = formatDate(basicInfo.user_date_last_login);
+  const lastLogout = formatDate(basicInfo.user_date_last_logout);
+  const isOnline = lastLogin > lastLogout;
+
   return (
-    <section className={styles["user-info"]}>
+    <section className={styles["user-info-container"]}>
+      <div className={styles["image-wrapper"]}>
+        <Image src="/images/ethi.png" alt="userinfo background" fill sizes="20rem" />
+      </div>
+
       {basicInfo && (
-        <div>
-          <h1>현재 접속중</h1>
-          <h1>{userName}</h1>
+        <div className={styles["user-info-list"]}>
+          <div className={styles["user-info-name-and-status"]}>
+            <h1 className={styles["user-name"]}>{userName}</h1>
+            <div className={styles["user-info-status"]}>
+              <div className={`${styles["status-circle"]} ${isOnline ? styles["online"] : styles["offline"]}`} />
+              <h1 className={isOnline ? styles["online-text"] : styles["offline-text"]}>
+                {isOnline ? "온라인" : "오프라인"}
+              </h1>
+            </div>
+          </div>
+          <dl>
+            <div className={styles["user-info-description"]}>
+              <div className={styles["user-info-level"]}>
+                <dt>레벨</dt>
+                <dd className="level">{basicInfo.user_level}</dd>
+              </div>
 
-          <p>길드: ----</p>
-          <p>닉네임: {basicInfo.user_name}</p>
+              <div className={styles["user-info-exp"]}>
+                <dt>경험치</dt>
+                <dd className="exp">{basicInfo.user_exp}</dd>
+              </div>
+            </div>
 
-          <p>
-            최근 로그인:
-            {new Date(basicInfo.user_date_last_login).toLocaleString("ko-KR", {
-              year: "numeric",
-              month: "2-digit",
-              day: "2-digit",
-              hour: "2-digit",
-              minute: "2-digit",
-              second: "2-digit",
-              hour12: false,
-            })}
-          </p>
-          {/* <p>최근 로그아웃: {new Date(basicInfo.user_date_last_logout).toLocaleString()}</p> */}
+            <div className={styles["user-info-description"]}>
+              <div className={styles["user-info-guild"]}>
+                <dt>길드</dt>
+                <dd>-------</dd>
+              </div>
 
-          {/* <p>가입일: {new Date(basicInfo.user_date_create).toLocaleString()}</p> */}
-          <p>경험치: {basicInfo.user_exp}</p>
-          <p>레벨: {basicInfo.user_level}</p>
+              <div className={styles["user-info-nickname"]}>
+                <dt>닉네임</dt>
+                <dd>{basicInfo.user_name ?? ""}</dd>
+              </div>
+            </div>
+
+            <div className={styles["user-info-time-stamp"]}>
+              <dt>최근 로그인</dt>
+              <dd>{lastLogin}</dd>
+            </div>
+
+            <div className={styles["user-info-time-stamp"]}>
+              <dt>최근 로그아웃</dt>
+              <dd>{isOnline ? "-" : lastLogout}</dd>
+            </div>
+
+            <div className={styles["user-info-time-stamp"]}>
+              <dt>캐릭터 생성일</dt>
+              <dd>{formatDate(basicInfo.user_date_create)}</dd>
+            </div>
+          </dl>
         </div>
       )}
     </section>
--- src/app/_lib/index.ts
+++ src/app/_lib/index.ts
@@ -1,3 +1,7 @@
 export function normalizeKey(s: string) {
   return s.trim().normalize("NFC");
 }
+
+export const formatDate = function (date: Date): string {
+  return new Date(date).toLocaleString("ko-KR");
+};
