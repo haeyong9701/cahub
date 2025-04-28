280482a5b1a8593352cbd9d2a3f057623a4ef420
haeyong9701 <haeyong9701@gmail.com>
2025-04-28 12:18:42 +0900

    feat: 이미지 없을 때 no-image 추가 & 로딩 인디케이터 추가

--- src/app/_components/ItemImage.tsx
+++ src/app/_components/ItemImage.tsx
@@ -2,14 +2,26 @@
 
 import Image from "next/image";
 import { useManifest } from "@/app/_query";
+import Spinner from "./Spinner";
 
 // ItemImage 컴포넌트: 각 아이템 이름당 한 번 훅 호출
 export function ItemImage({ itemName }: { itemName: string }) {
-  const { data, isPending, isError } = useManifest(itemName);
+  const { data, isPending, isError, isSuccess } = useManifest(itemName);
 
-  if (isPending) return <p>이미지 로딩…</p>;
+  if (isPending) return <Spinner />;
+
   if (isError) return <p style={{ color: "red" }}>이미지 로딩 실패</p>;
-  if (!data || data.length === 0) return <p>아이템 이미지 없음</p>;
+
+  if (isSuccess && data.length === 0)
+    return (
+      <Image
+        src="/images/no-image-placeholder.png"
+        alt="아이템 이미지 없음"
+        width={65}
+        height={65}
+        style={{ objectFit: "contain" }}
+      />
+    );
 
   const src = data[0].item_src;
   const isGif = /\.gif($|\?)/i.test(src);
---
+++ src/app/_components/Spinner/index.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import styles from "./Spinner.module.scss";
+
+export default function Spinner() {
+  return <div className={styles["spinner"]}></div>;
+}
