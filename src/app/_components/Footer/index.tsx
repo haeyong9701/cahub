bbb7f8b36c53b58f3b15e6618d2e43c074a9c6ad
haeyong9701 <haeyong9701@gmail.com>
2025-04-28 12:53:17 +0900

    style: footer 연락처 추가, 폰트 크기 변경, 아이템 박스 배경색 변경

--- src/app/_components/Footer/index.tsx
+++ src/app/_components/Footer/index.tsx
@@ -5,6 +5,7 @@ import styles from "./Footer.module.scss";
 export default function Footer() {
   return (
     <footer className={styles["footer"]}>
+      <p>contact : cahubxyz@gmail.com</p>
       <p>cahub.xyz is not associated with NEXON Korea. Data based on NEXON Open API.</p>
     </footer>
   );
