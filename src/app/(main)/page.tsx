581b8f022757728f2d1ce250b1cb3dbabef705ef
haeyong9701 <haeyong9701@gmail.com>
2025-04-30 22:48:36 +0900

    feat: 메인 페이지 이미지 priority 속성 추가

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function MainPage() {
     <div className={styles["main-page-container"]}>
       <section className={styles["search-section"]}>
         <div className={styles["logo-wrapper"]}>
-          <Image src="/images/logo.png" alt="CAHUB" fill sizes="(min-width: 768px) 350px, 250px" />
+          <Image src="/images/logo.png" alt="CAHUB" fill sizes="(min-width: 768px) 350px, 250px" priority />
         </div>
         <div className={styles["description"]}>
           <p>크레이지아케이드 유저 정보 조회</p>
