481688434bdd6517e36c62bff3e4376750db7a23
haeyong9701 <haeyong9701@gmail.com>
2025-05-01 10:14:12 +0900

    delete: 안쓰는 유틸 함수(nomalizeKey) 삭제

--- src/utils/app/index.tsx
+++ src/utils/app/index.tsx
@@ -1,5 +1,4 @@
 export { formatDateKoreanMinute } from "./formatDateKorean";
 export { getNextLevelInfo } from "./getNextLevelInfo";
-export { normalizeKey } from "./normalizeKey";
 export { getUserStatusInfo } from "./getUserStatusInfo";
 export { calculateExpRatio } from "./calculateExpRatio";
--- src/utils/app/normalizeKey.ts
+++
@@ -1,3 +0,0 @@
-export function normalizeKey(s: string) {
-  return s.trim().normalize("NFC");
-}
