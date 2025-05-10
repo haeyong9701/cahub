78321f4e0a0364effe0775d79fad617c926e204a
haeyong9701 <haeyong9701@gmail.com>
2025-05-10 18:24:26 +0900

    delete: Vercel 하이드레이션 에러 해결을 위해 구글 애널리틱스 삭제

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,12 +1,10 @@
 import { Geist } from "next/font/google";
 import type { Metadata } from "next";
 import Script from "next/script";
-import { Suspense } from "react";
 import "@/styles/globals.scss";
 import QueryProvider from "@/components/QueryProvider";
 import Navbar from "@/components/Navbar/Navbar";
 import Footer from "@/components/Footer/Footer";
-import GoogleAnalytics from "@/components/GoogleAnalytics";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -24,30 +22,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   const isProduction = process.env.NODE_ENV === "production";
-  const GA_ID = process.env.NEXT_PUBLIC_GA_MEASUREMENT_ID;
   const NEXON_APP_ID = process.env.NEXON_APP_ID;
 
   return (
     <html lang="ko">
       <head>
-        {/* 1) GA 라이브러리 로드 */}
-        {isProduction && GA_ID && (
-          <Script src={`https://www.googletagmanager.com/gtag/js?id=${GA_ID}`} strategy="afterInteractive" />
-        )}
-        {/* 2) gtag 초기화 */}
-        {isProduction && GA_ID && (
-          <Script id="ga-init" strategy="afterInteractive">
-            {`
-              window.dataLayer = window.dataLayer || [];
-              function gtag(){dataLayer.push(arguments);}
-              gtag('js', new Date());
-              gtag('config', '${GA_ID}', {
-                page_path: window.location.pathname,
-              });
-            `}
-          </Script>
-        )}
-
         {isProduction && NEXON_APP_ID && (
           <Script
             src={`https://openapi.nexon.com/js/analytics.js?app_id=${NEXON_APP_ID}`}
@@ -58,10 +37,6 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       </head>
 
       <body className={`${geistSans.variable} `}>
-        <Suspense fallback={null}>
-          <GoogleAnalytics />
-        </Suspense>
-
         <QueryProvider>
           <Navbar />
           {children}
